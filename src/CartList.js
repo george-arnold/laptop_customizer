24deac215221f55a4bb334222affe0f09ffc3f40
George Arnold <george.arnold.88@gmail.com>
2019-12-28 14:38:42 -0500

    broke code into two components...also broke code

--- src/CartList.js
+++ src/CartList.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import CartSelected from './CartSelected';
+import CartOptions from './CartSelected';
 import slugify from 'slugify';
 
 class CartList extends Component {
-  
+  render () {
+    const summary = Object.keys(this.state.selected).map((feature, idx) => {
+      const featureHash = feature + '-' + idx;
+      const selectedOption = this.state.selected[feature];
+
+      return (
+        <div className="summary__option" key={featureHash}>
+          <div className="summary__option__label">{feature} </div>
+          <div className="summary__option__value">{selectedOption.name}</div>
+          <div className="summary__option__cost">
+            {USCurrencyFormat.format(selectedOption.cost)}
+          </div>
+        </div>
+      );
+    });
+}
 }
 export default CartList; 
