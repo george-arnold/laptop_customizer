a3db9f833fd7d6c3c8d254fee2ac866938334e35
George Arnold <george.arnold.88@gmail.com>
2019-12-29 15:34:11 -0500

    Tried to organize into parts

--- src/PartList.js
+++ src/PartList.js
@@ -14,19 +14,7 @@ class PartList extends Component{
     const options = this.props.features[feature].map(item => {
       const itemHash = slugify(JSON.stringify(item));
       return (
-        <div key={itemHash} className="feature__item">
-          <input
-            type="radio"
-            id={itemHash}
-            className="feature__option"
-            name={slugify(feature)}
-            checked={item.name === this.state.selected[feature].name}
-            onChange={e => this.updateFeature(feature, item)}
-          />
-          <label htmlFor={itemHash} className="feature__label">
-            {item.name} ({USCurrencyFormat.format(item.cost)})
-          </label>
-        </div>
+       <PartOptions />
       );
     });
 
@@ -39,6 +27,6 @@ class PartList extends Component{
       </fieldset>
     );
   });
-}}
-
+}
+}
 export default PartList;
