e23ba0e53048069b76231c357eebaa0ccdd8e732
George Arnold <george.arnold.88@gmail.com>
2019-12-29 18:06:33 -0500

    changed stated to props

--- src/PartOptions.js
+++ src/PartOptions.js
@@ -18,7 +18,7 @@ class PartOptions extends Component {
               id={itemHash}
               className="feature__option"
               name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
+              checked={item.name === this.props.selected[feature].name}
               onChange={e => this.updateFeature(feature, item)}
             />
             <label htmlFor={itemHash} className="feature__label">
